c243f5ee59cb41e205746421ee1a88202a1c0c2a
Mike Exner-Kittridge <Mike.Exner-Kittridge@ecan.govt.nz>
2018-03-27 09:06:28 +1300

    modified the init

--- pyhydllp/__init__.py
+++ pyhydllp/__init__.py
@@ -1,3 +1,6 @@
-from pyhydllp import hydllp, combo, util, sql
+try:
+    from pyhydllp import hydllp, combo, util, sql
+except ImportError:
+    from pyhydllp import hydllp, util
 from pyhydllp.hyd import hyd
 
