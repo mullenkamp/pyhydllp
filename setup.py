e074eb62dd7bda7135cd6c7ad6428eec1728598c
MikeK <mgkittridge@gmail.com>
2018-09-19 12:02:30 +1200

    update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ version = '1.0.11'
 if os.environ.get('READTHEDOCS', False) == 'True':
     INSTALL_REQUIRES = []
 else:
-    INSTALL_REQUIRES = ['pandas']
+    INSTALL_REQUIRES = ['pandas', 'pdsql']
 
 # Get the long description from the README file
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
